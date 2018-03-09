5dae976048d9c137e53146b153a7840bec3f75a7
Chris West (Faux) <git@goeswhere.com>
2018-03-09 10:20:37 +0000

    move to nom4

--- src/expr/parse.rs
+++ src/expr/parse.rs
@@ -6,6 +6,7 @@ use std::num::ParseIntError;
 use std::result::Result as StdResult;
 use std::str::FromStr;
 
+use nom::types::CompleteStr;
 use nom::IResult;
 use nom::multispace;
 
@@ -16,31 +17,31 @@ use expr::Expression;
 use expr::Input;
 use expr::Op;
 
-named!(input<&str, Input>, alt_complete!(
+named!(input<CompleteStr, Input>, alt_complete!(
     tag!("src") => { |_| Input::Src } |
     tag!("dst") => { |_| Input::Dst }
 ));
 
-named!(op<&str, Op>, alt_complete!(
+named!(op<CompleteStr, Op>, alt_complete!(
     tag!("eq") => { |_| Op::Eq } |
     tag!("=") => { |_| Op::Eq } |
     tag!("==") => { |_| Op::Eq }
 ));
 
-fn parse_u8(input: &str) -> StdResult<u8, ParseIntError> {
-    input.parse()
+fn parse_u8(input: CompleteStr) -> StdResult<u8, ParseIntError> {
+    input.0.parse()
 }
 
-fn parse_u16(input: &str) -> StdResult<u16, ParseIntError> {
-    input.parse()
+fn parse_u16(input: CompleteStr) -> StdResult<u16, ParseIntError> {
+    input.0.parse()
 }
 
-fn parse_quad(input: &str) -> StdResult<u16, ParseIntError> {
-    u16::from_str_radix(input, 16)
+fn parse_quad(input: CompleteStr) -> StdResult<u16, ParseIntError> {
+    u16::from_str_radix(input.0, 16)
 }
 
-fn parse_v6(input: &str) -> StdResult<IpAddr, AddrParseError> {
-    Ok(input.parse::<Ipv6Addr>()?.into())
+fn parse_v6(input: CompleteStr) -> StdResult<IpAddr, AddrParseError> {
+    Ok(input.0.parse::<Ipv6Addr>()?.into())
 }
 
 fn digit(input: char) -> bool {
@@ -51,20 +52,20 @@ fn hex_digit(input: char) -> bool {
     input.is_ascii_hexdigit()
 }
 
-named!(port<&str, u16>, preceded!(complete!(tag!(":")), alt_complete!(
+named!(port<CompleteStr, u16>, preceded!(complete!(tag!(":")), alt_complete!(
     map_res!(take_while1_s!(digit), parse_u16) |
     tag!("*") => { |_| 0 }
 )));
 
 // TODO: I'd love `do_parse!()` instead of this `map_res!` and helper nonsense,
 // TODO: but it doesn't seem to consume the input, and I don't get why.
-named!(octet<&str, u8>, map_res!(take_while1_s!(digit), parse_u8));
+named!(octet<CompleteStr, u8>, map_res!(take_while1_s!(digit), parse_u8));
 
-named!(quad<&str, u16>, map_res!(take_while1_s!(hex_digit), parse_quad));
+named!(quad<CompleteStr, u16>, map_res!(take_while1_s!(hex_digit), parse_quad));
 
-named!(mask<&str, u8>, preceded!(complete!(tag!("/")), octet));
+named!(mask<CompleteStr, u8>, preceded!(complete!(tag!("/")), octet));
 
-named!(v4addr<&str, IpAddr>, do_parse!(
+named!(v4addr<CompleteStr, IpAddr>, do_parse!(
     a: octet >>
     tag!(".") >>
     b: octet >>
@@ -75,7 +76,7 @@ named!(v4addr<&str, IpAddr>, do_parse!(
     ( Ipv4Addr::new(a, b, c, d).into() )
 ));
 
-named!(v6addr_full<&str, IpAddr>, do_parse!(
+named!(v6addr_full<CompleteStr, IpAddr>, do_parse!(
     a: quad >>
     b: count_fixed!(
         u16,
@@ -85,30 +86,30 @@ named!(v6addr_full<&str, IpAddr>, do_parse!(
     ( Ipv6Addr::new(a, b[0], b[1], b[2], b[3], b[4], b[5], b[6]).into() )
 ));
 
-named!(v6_quads<&str, Vec<u16>>, separated_list_complete!(
+named!(v6_quads<CompleteStr, Vec<u16>>, separated_list_complete!(
     tag!(":"),
     quad
 ));
 
 // TODO: could take the actual parsed u16s out of this, recombine them by hand
 // TODO: if the length was right, and build the address directly
-named!(v6addr_abbr_match<&str, &str>, recognize!(do_parse!(
-    before: v6_quads >>
+named!(v6addr_abbr_match<CompleteStr, CompleteStr>, recognize!(do_parse!(
+    _before: v6_quads >>
     tag!("::") >>
-    after: v6_quads >>
+    _after: v6_quads >>
     ()
 )));
 
-named!(v6addr_abbr<&str, IpAddr>,
+named!(v6addr_abbr<CompleteStr, IpAddr>,
     map_res!(v6addr_abbr_match, parse_v6)
 );
 
-named!(v6addr<&str, IpAddr>, alt_complete!(
+named!(v6addr<CompleteStr, IpAddr>, alt_complete!(
     v6addr_abbr |
     v6addr_full
 ));
 
-named!(addr<&str, IpAddr>, alt_complete!(
+named!(addr<CompleteStr, IpAddr>, alt_complete!(
     v4addr |
     delimited!(
         tag!("["),
@@ -120,7 +121,7 @@ named!(addr<&str, IpAddr>, alt_complete!(
     )
 ));
 
-named!(amp_addr_opt_opt<&str, AddrMaskPort>, do_parse!(
+named!(amp_addr_opt_opt<CompleteStr, AddrMaskPort>, do_parse!(
     addr: addr >>
     mask: opt!(mask) >>
     port: opt!(port) >>
@@ -129,19 +130,19 @@ named!(amp_addr_opt_opt<&str, AddrMaskPort>, do_parse!(
     } )
 ));
 
-named!(amp_just_port<&str, AddrMaskPort>, do_parse!(
+named!(amp_just_port<CompleteStr, AddrMaskPort>, do_parse!(
     port: port >>
     ( AddrMaskPort {
         addr: None, mask: None, port: Some(port)
     } )
 ));
 
-named!(addr_mask_port<&str, AddrMaskPort>, alt_complete!(
+named!(addr_mask_port<CompleteStr, AddrMaskPort>, alt_complete!(
     amp_addr_opt_opt |
     amp_just_port
 ));
 
-named!(addr_expr<&str, Expression>, do_parse!(
+named!(addr_expr<CompleteStr, Expression>, do_parse!(
     input: input >>
     many1!(multispace) >>
     op: op >>
@@ -149,6 +150,10 @@ named!(addr_expr<&str, Expression>, do_parse!(
     addr: addr_mask_port >>
     ( Expression::Addr(AddrFilter { input, op, addr })) )
 );
+
+fn explain(result: IResult<CompleteStr, Expression>) -> String {
+    format!("{:?}", result)
+}
 
 #[cfg(test)]
 mod tests {
@@ -160,95 +165,101 @@ mod tests {
 
     #[test]
     fn parts() {
-        assert_eq!(IResult::Done("", 2), octet("2"));
-        assert_eq!(IResult::Done("", 2), port(":2"));
-        assert_eq!(IResult::Done("", 4), mask("/4"));
-        assert_eq!(IResult::Done("", 12), mask("/12"));
+        assert_eq!(Ok((CompleteStr(""), 2)), octet(CompleteStr("2")));
+        assert_eq!(Ok((CompleteStr(""), 2)), port(CompleteStr(":2")));
+        assert_eq!(Ok((CompleteStr(""), 4)), mask(CompleteStr("/4")));
+        assert_eq!(Ok((CompleteStr(""), 12)), mask(CompleteStr("/12")));
         assert_eq!(
-            IResult::Done("", "0.0.0.0".parse::<Ipv4Addr>().unwrap().into()),
-            addr("0.0.0.0")
+            Ok((CompleteStr(""), "0.0.0.0".parse::<Ipv4Addr>().unwrap().into())),
+            addr(CompleteStr("0.0.0.0"))
         );
         assert_eq!(
-            IResult::Done("", "::".parse::<Ipv6Addr>().unwrap().into()),
-            addr("[::]")
+            Ok((CompleteStr(""), "::".parse::<Ipv6Addr>().unwrap().into())),
+            addr(CompleteStr("[::]"))
         );
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 "2001:db8:0:0:1::1".parse::<Ipv6Addr>().unwrap().into()
-            ),
-            addr("[2001:db8::1:0:0:1]")
+            )),
+            addr(CompleteStr("[2001:db8::1:0:0:1]"))
         );
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 "2001:db8:0:0:1:0:0:1".parse::<Ipv6Addr>().unwrap().into()
-            ),
-            addr("[2001:db8:0:0:1:0:0:1]")
+            )),
+            addr(CompleteStr("[2001:db8:0:0:1:0:0:1]"))
         );
         assert_eq!(
-            IResult::Done("", AMP::new_str_v4(Some("0.0.0.0"), None, None)),
-            amp_addr_opt_opt("0.0.0.0")
+            Ok((CompleteStr(""), AMP::new_str_v4(Some("0.0.0.0"), None, None))),
+            amp_addr_opt_opt(CompleteStr("0.0.0.0"))
         );
     }
 
     #[test]
     fn addr_expr_full() {
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 E::Addr(AF {
                     input: Input::Src,
                     op: Op::Eq,
                     addr: AMP::new_str_v4(Some("0.0.0.0"), Some(0), Some(80)),
                 })
-            ),
-            addr_expr("src eq 0.0.0.0/0:80")
+            )),
+            addr_expr(CompleteStr("src eq 0.0.0.0/0:80"))
         );
     }
 
     #[test]
     fn addr_expr_no_mask() {
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 E::Addr(AF {
                     input: Input::Src,
                     op: Op::Eq,
                     addr: AMP::new_str_v4(Some("0.0.0.0"), None, Some(80)),
                 })
-            ),
-            addr_expr("src eq 0.0.0.0:80")
+            )),
+            addr_expr(CompleteStr("src eq 0.0.0.0:80"))
         );
     }
 
     #[test]
     fn addr_expr_no_port() {
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 E::Addr(AF {
                     input: Input::Src,
                     op: Op::Eq,
                     addr: AMP::new_str_v4(Some("0.0.0.0"), None, None),
                 })
-            ),
-            addr_expr("src eq 0.0.0.0")
+            )),
+            addr_expr(CompleteStr("src eq 0.0.0.0"))
         );
     }
 
     #[test]
     fn addr_expr_no_addr() {
         assert_eq!(
-            IResult::Done(
-                "",
+            Ok((
+                CompleteStr(""),
                 E::Addr(AF {
                     input: Input::Src,
                     op: Op::Eq,
                     addr: AMP::new_str_v4(None, None, Some(80)),
                 })
-            ),
-            addr_expr("src eq :80")
+            )),
+            addr_expr(CompleteStr("src eq :80"))
         );
+    }
+
+    #[ignore]
+    #[test]
+    fn error_bad_addr() {
+        assert_eq!("nope", explain(addr_expr(CompleteStr("src eq _"))));
     }
 }
